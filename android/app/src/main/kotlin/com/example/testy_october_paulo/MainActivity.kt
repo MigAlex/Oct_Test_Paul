67b5effa8b7bbe5e3f342124fcdf2ebf8e3466b6
Alex <awangardowie@gmail.com>
2019-10-18 14:18:50 +0200

    1st savings

---
+++ android/app/src/main/kotlin/com/example/testy_october_paulo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testy_october_paulo
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
